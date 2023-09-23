c50efcfe2dc92ad570cad7496f0e12348d7e3af1
Roman Kuchmii <romakuchmii@gmail.com>
2023-09-23 11:08:07 +0300

    added empty line

--- src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.example.onlinebookstore.dto.user.UserResponseDto;
 import com.example.onlinebookstore.exception.RegistrationException;
 import com.example.onlinebookstore.service.AuthenticationService;
 import com.example.onlinebookstore.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User authentication",description = "Endpoints for managing users")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
@@ -22,11 +25,13 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
+    @Operation(summary = "login registered user",description = "login registered user")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
         return authenticationService.authenticate(request);
     }
 
     @PostMapping("/register")
+    @Operation(summary = "register user",description = "register user")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
